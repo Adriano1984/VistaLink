c625d6d7d0a24ee1c75056fcda6a5b6335a77f7d
Adriano1984 <adriaan.vanerps@gmail.com>
2019-09-22 20:24:46 +0200

    Add files via upload
    Generate ViDi ReadResults to reuse original VPro ToolBlock

---
+++ VidiReadResults.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidiReadResults
+{
+   [Serializable]
+    public class VidiReadResults
+
+    {
+
+        public char Character { get; set; }
+
+        public double Score { get; set; }
+
+        public double Position { get; set; }
+
+        public VidiReadResults(char character, double score, double position)
+
+        {
+
+            Character = character;
+
+            Score = score;
+
+            Position = position;
+
+        }
+
+        public string GetDetails()
+
+        {
+            string details = Character.ToString() + " " + Score.ToString() + " " + Position.ToString();
+            return details;
+
+
+        }
+    }
+}
